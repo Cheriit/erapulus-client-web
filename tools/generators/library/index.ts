dacb73ce63f3fc9f338e7a45c061b9bd23a80113
Filip 'Cherit' Szóstak <f.szostak44@gmail.com>
2021-12-03 19:43:16 +0100

    Added generator

---
+++ tools/generators/library/index.ts
@@ -0,0 +1,22 @@
+import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
+import { libraryGenerator } from '@nrwl/workspace/generators';
+
+export default async function (tree: Tree, schema: any) {
+  await libraryGenerator(tree, {
+    name: schema.name,
+    skipFormat: false,
+    buildable: true,
+    directory: `${schema.type}`,
+    importPath: `@erapulus/${schema.name}`,
+    linter: 'eslint',
+    simpleModuleName: true,
+    tags: `${schema.type}`,
+    unitTestRunner: 'jest',
+    strict: true,
+    testEnvironment: 'node',
+  });
+  await formatFiles(tree);
+  return () => {
+    installPackagesTask(tree);
+  };
+}
