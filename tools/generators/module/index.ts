a810ecd4751486f33c034a324b06a01c9e71148e
Filip 'Cherit' Szóstak <f.szostak44@gmail.com>
2021-12-04 14:07:07 +0100

    Added module generator

---
+++ tools/generators/module/index.ts
@@ -0,0 +1,16 @@
+import { Tree, formatFiles } from '@nrwl/devkit';
+import {wrapAngularDevkitSchematic} from "@nrwl/tao/src/commands/ngcli-adapter";
+
+export default async function (tree: Tree, schema: any) {
+  const schematic =  wrapAngularDevkitSchematic('@schematics/angular', 'component');
+  await schematic(tree, {
+    "routing": schema.route,
+    "route": schema.route? schema.routeName : undefined,
+    "name": schema.name,
+    "module": schema.module,
+    "project": schema.project
+  })
+  await formatFiles(tree);
+  return () => {
+  };
+}
