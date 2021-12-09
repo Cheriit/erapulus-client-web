21982ae8889ff56b8eb7762f19ec28a99c62ddca
Filip 'Cherit' Szóstak <f.szostak44@gmail.com>
2021-12-09 22:33:42 +0100

    Updated Tailwind to 3.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,19 +5,13 @@ process.env.TAILWIND_MODE = guessProductionMode() ? 'build' : 'watch';
 module.exports = {
   prefix: '',
   mode: 'jit',
-  purge: {
-    enabled: guessProductionMode(),
-    content: [
-      './apps/**/*.{html,ts,css,scss,sass,less,styl}',
-      './libs/**/*.{html,ts,css,scss,sass,less,styl}',
-    ]
-  },
-  darkMode: 'media', // or 'media' or 'class'
+  content: [
+    './apps/**/*.{html,component.ts,css,scss,sass,less,styl}',
+    './libs/**/*.{html,ts,css,scss,sass,less,styl}',
+  ],
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
   plugins: [require('@tailwindcss/forms')],
+  ...(guessProductionMode() ? {cssnano: {}} : {})
 };
